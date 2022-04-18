80722a31f997e77d7c0982a673342e66e968f445
Domingos Alexandre <domingos_alexandre@d.alexandre.jrInspiron-3501>
2022-04-18 15:10:31 -0300

    INICIO

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { userState } from 'react';
+import AppContext from './context/App.Context';
 
 function App() {
+
+  const [ ] = userState([]);
+
+  const contexto = {
+
+  }
+
   return (
-    <span>Hello, App!</span>
+    <AppContext.Provider value={contexto}>
+      <span>Hello, App!</span>
+    </AppContext.Provider>
   );
 }
 
